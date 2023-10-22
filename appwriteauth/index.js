4b8ed0714a058a8866c4d9f71723ec47d0efa5d2
yuvraj-0777 <yuvsinh0777@gmail.com>
2023-10-23 00:26:37 +0530

    Adjustments Required

--- appwriteauth/index.js
+++ appwriteauth/index.js
@@ -3,7 +3,11 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './src/App';
+// import App from './src/screens/Home';
 import {name as appName} from './app.json';
+import Home from './src/screens/Home';
+import { Router } from './src/routes/Router';
+import { AppStack } from './src/routes/AppStack';
+import { AuthStack } from './src/routes/AuthStack';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Router);
